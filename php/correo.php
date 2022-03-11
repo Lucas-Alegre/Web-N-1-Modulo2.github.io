c1af44880ef58b6b33db2d7c73094cd1fdce7bf5
Lucas Alegre <lucasealegre123@gmail.com>
2022-03-11 11:26:16 -0300

    Solusion4

--- php/correo.php
+++ php/correo.php
@@ -13,6 +13,13 @@ mail($destinatario, $asunto, $mensajeCompleto, $header);
 /*echo "<script>alert('Enviado exitosamente')</script>";
 echo "<script> setTimeout(\"Location:https://lucas2022.github.io/Web-N-1-Modulo2.github.io/php/contact.html\", 1000) </script>";
 */
+$host = $_SERVER['HTTP_HOST'];
+$ruta = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$html = 'contact.html';
+$url = "http://$host$ruta/$html";
 
+header("Location: $url");
+include redirect.php;
+die();
 
 ?>
