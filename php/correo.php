b5df09e1e161bbbdda28a38fc47224e3ad074cd8
Lucas Alegre <lucasealegre123@gmail.com>
2022-03-11 11:17:35 -0300

    Solusion 3

--- php/correo.php
+++ php/correo.php
@@ -10,9 +10,9 @@ $header = "Enviado desde la pagina de contacto";
 $mensajeCompleto = $mensaje . "\nAtentamente ". $nombre ;
 
 mail($destinatario, $asunto, $mensajeCompleto, $header);
-echo "<script>alert('Enviado exitosamente')</script>";
+/*echo "<script>alert('Enviado exitosamente')</script>";
 echo "<script> setTimeout(\"Location:https://lucas2022.github.io/Web-N-1-Modulo2.github.io/php/contact.html\", 1000) </script>";
-
+*/
 
 
 ?>
