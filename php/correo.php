747814368116a99d16468ae369e5839153c96b4c
Lucas Alegre <lucasealegre123@gmail.com>
2022-03-11 11:13:21 -0300

    Solucion 2

--- php/correo.php
+++ php/correo.php
@@ -11,7 +11,7 @@ $mensajeCompleto = $mensaje . "\nAtentamente ". $nombre ;
 
 mail($destinatario, $asunto, $mensajeCompleto, $header);
 echo "<script>alert('Enviado exitosamente')</script>";
-echo "<script> setTimeout(\"Location:contact.html\", 1000) </script>";
+echo "<script> setTimeout(\"Location:https://lucas2022.github.io/Web-N-1-Modulo2.github.io/php/contact.html\", 1000) </script>";
 
 
 
