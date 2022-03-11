2af72727820634f32e931059aeddb3d785824c7c
Lucas Alegre <lucasealegre123@gmail.com>
2022-03-11 11:31:54 -0300

    sOLUCION5

--- php/correo.php
+++ php/correo.php
@@ -7,19 +7,11 @@ $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 
 $header = "Enviado desde la pagina de contacto";
-$mensajeCompleto = $mensaje . "\nAtentamente ". $nombre ;
+$mensajeCompleto = $mensaje . "\n Atentamente ". $nombre ;
 
 mail($destinatario, $asunto, $mensajeCompleto, $header);
-/*echo "<script>alert('Enviado exitosamente')</script>";
-echo "<script> setTimeout(\"Location:https://lucas2022.github.io/Web-N-1-Modulo2.github.io/php/contact.html\", 1000) </script>";
-*/
-$host = $_SERVER['HTTP_HOST'];
-$ruta = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$html = 'contact.html';
-$url = "http://$host$ruta/$html";
+echo "<script>alert('Enviado exitosamente')</script>";
+echo "<script> setTimeout(\"Location:index.html\", 1000) </script>";
 
-header("Location: $url");
-include redirect.php;
-die();
 
 ?>
